ec933dde431b82fd7b061df57df28fe2fe762919
joanna <you@example.com>
2016-02-09 16:01:40 +0100

    img slider in section1

---
+++ static/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+  anchors: ['page1', 'page2', 'page3', 'page4'],
+  //sectionsColor: ['yellow', 'orange', '#C0C0C0', '#ADD8E6'],
+});
+
+//adding the action to the button
+$(document).on('click', '#moveRight', function(){
+  $.fn.fullpage.moveSlideRight();
+});
+});
